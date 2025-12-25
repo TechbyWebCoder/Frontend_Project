3d6c6adf41e7529a9ccadac8d6dbca4d3501b6d5
TechbyWebCoder <techbywedcoder@gmail.com>
2025-12-25 18:20:12 +0530

    Add files via upload

---
+++ 20.Python Quiz/python.js
@@ -0,0 +1,105 @@
+const quizData = [
+    {
+        question: "Which keyword is used to define a function in Python?",
+        a: "def",
+        b: "func",
+        c: "define",
+        d: "function",
+        correct: "a",
+    },
+    {
+        question: "How do you declare a variable in Python?",
+        a: "variable = 5",
+        b: "int variable = 5",
+        c: "let variable = 5",
+        d: "var variable = 5",
+        correct: "a",
+    },
+    {
+        question: "What is the default value of a boolean variable in Python?",
+        a: "True",
+        b: "False",
+        c: "0",
+        d: "None",
+        correct: "b",
+    },
+    {
+        question: "Which method is used to get the length of a list in Python?",
+        a: "length()",
+        b: "size()",
+        c: "count()",
+        d: "len()",
+        correct: "d",
+    },
+    {
+        question: "What is the purpose of the 'self' keyword in Python?",
+        a: "To refer to the instance of the class",
+        b: "To define a class method",
+        c: "To create a new class",
+        d: "To indicate a private method",
+        correct: "a",
+    }
+];
+
+const quiz = document.getElementById('quiz');
+const answerEls = document.querySelectorAll('.answer');
+const questionEl = document.getElementById('question');
+const a_text = document.getElementById('a_text');
+const b_text = document.getElementById('b_text');
+const c_text = document.getElementById('c_text');
+const d_text = document.getElementById('d_text');
+const submitBtn = document.getElementById('submit');
+
+let currentQuiz = 0;
+let score = 0;
+
+loadQuiz();
+
+function loadQuiz() {
+    deselectAnswers();
+
+    const currentQuizData = quizData[currentQuiz];
+
+    questionEl.innerText = currentQuizData.question;
+    a_text.innerText = currentQuizData.a;
+    b_text.innerText = currentQuizData.b;
+    c_text.innerText = currentQuizData.c;
+    d_text.innerText = currentQuizData.d;
+}
+
+function deselectAnswers() {
+    answerEls.forEach(answerEl => answerEl.checked = false);
+}
+
+function getSelected() {
+    let answer;
+
+    answerEls.forEach(answerEl => {
+        if(answerEl.checked) {
+            answer = answerEl.id;
+        }
+    });
+
+    return answer;
+}
+
+submitBtn.addEventListener('click', () => {
+    const answer = getSelected();
+    
+    if(answer) {
+        if(answer === quizData[currentQuiz].correct) {
+            score++;
+        }
+
+        currentQuiz++;
+
+        if(currentQuiz < quizData.length) {
+            loadQuiz();
+        } else {
+            quiz.innerHTML = `
+                <h2>You answered ${score}/${quizData.length} questions correctly</h2>
+                <button onclick="location.reload()">Reload</button>
+            `;
+        }
+    }
+});
